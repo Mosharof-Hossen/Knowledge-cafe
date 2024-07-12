571b39380cc0d46b5c9899290334cf63e80b2ca0
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-07-12 12:11:44 +0600

    face data generated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-red-700 text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Header></Header>
     </>
   )
 }
