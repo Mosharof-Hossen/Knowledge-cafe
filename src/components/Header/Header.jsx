bc540f8a06628c5b6aa9daccba2613030d28b702
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-07-12 17:28:08 +0600

    header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
-
-
+import { useEffect } from "react";
+import { useState } from "react";
+import profile from '../../assets/images/profile.png'
 const Header = () => {
+
+    const [blogs, setBlogs] = useState([]);
+    useEffect(()=>{
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    },[])
+
+    console.log(blogs);
     return (
-        <div>
-            <h2>Header</h2>
+        <div className="flex justify-between items-center p-4 mx-4 border-b-2">
+            <h2 className="text-4xl font-bold">Knowledge Cafe</h2>
+            <img src={profile} alt=""  />
         </div>
     );
 };
